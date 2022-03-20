4009e862efcb145e634382eaf33f803cd94ee1e2
Thiago Oliveira <thiago.oliveira@consorcioluiza.com.br>
2022-03-20 14:35:45 -0300

    upload arquivos para s3

--- s3_upload.py
+++ s3_upload.py
@@ -9,7 +9,7 @@ dirname = os.path.dirname(__file__)
 ACCESS_ID=os.getenv('ACCESS_ID')
 ACCESS_KEY=os.getenv('ACCESS_KEY')
 DATA_DIR=os.path.join(dirname, os.getenv('DATA_DIR', 'dados_rais'))
-
+BUCKET=os.getenv('BUCKET')
 
 def get_files_to_upload(dir):
     files_to_upload = list()
@@ -25,3 +25,12 @@ def get_files_to_upload(dir):
     return files_to_upload
 
 files_to_upload = get_files_to_upload(DATA_DIR)
+s3_client = boto3.client('s3', aws_access_key_id=ACCESS_ID, aws_secret_access_key=ACCESS_KEY)
+
+files_qtd = len(files_to_upload)
+for index, file_path in enumerate(files_to_upload):
+    file = file_path.split(sep="\\").pop(-1)
+    s3_client.upload_file(file_path, BUCKET, f'raw-data/rais/year=2020/{file}')
+    print(f'Uploaded {file} {index + 1} of {files_qtd}')
+
+print(f'Upload {files_qtd}')
